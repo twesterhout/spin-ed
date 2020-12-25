9d636435f913b53a49534508991c90d4c2a6a2c1
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2020-12-25 17:13:13 +0100

    Format using shfmt

--- build_lattice_symmetries.sh
+++ build_lattice_symmetries.sh
@@ -2,19 +2,23 @@
 
 set -e
 
-TEST=0
-BUILD=1
 SHARED=0
+BUILD=1
 while [[ $# -gt 0 ]]; do
 	key="$1"
 	case $key in
-		--test) TEST=1; shift; ;;
-		--shared) SHARED=1; shift; ;;
-		--clean) BUILD=0; shift; ;;
-		*)
-			echo "Unknown argument: $1"
-			echo "Usage: ./build_lattice_symmetries.sh [--test] [--shared] [--clean]"
-			exit 1
+	--shared)
+		SHARED=1
+		shift
+		;;
+	--clean)
+		BUILD=0
+		shift
+		;;
+	*)
+		echo "Unknown argument: '$1'"
+		echo "Usage: build_lattice_symmetries.sh [--shared] [--clean]"
+		exit 1
 		;;
 	esac
 done
@@ -24,7 +28,7 @@ WORKDIR="$PWD/third_party/build"
 
 mkdir -p "$WORKDIR" && cd "$WORKDIR"
 if [ ! -d "lattice-symmetries" ]; then
-	[ $VERBOSE -eq 1 ] && echo "Downloading lattice-symmetries"
+	echo "Downloading lattice-symmetries..."
 	git clone --depth 1 git@github.com:twesterhout/lattice-symmetries
 fi
 cd "lattice-symmetries"
@@ -34,15 +38,16 @@ git submodule update --init --recursive
 if [ $BUILD -eq 1 ]; then
 	mkdir -p build && cd build
 
-	export CXXFLAGS="-march=nocona -mtune=ivybridge"
+	export CXXFLAGS="-fvisibility-inlines-hidden -march=nocona -mtune=haswell -fstack-protector-strong -fno-plt -ffunction-sections"
+	export LDFLAGS="-Wl,--as-needed -Wl,--gc-sections -Wl,-z,now -Wl,--disable-new-dtags"
 	cmake -GNinja -DCMAKE_INSTALL_PREFIX=$PREFIX \
-				  -DCMAKE_BUILD_TYPE=Debug \
-				  -DBUILD_SHARED_LIBS=$SHARED \
-				  -DLatticeSymmetries_ENABLE_UNIT_TESTING=$TEST \
-				  ..
+		-DCMAKE_BUILD_TYPE=Release \
+		-DBUILD_SHARED_LIBS=$SHARED \
+		-DLatticeSymmetries_ENABLE_UNIT_TESTING=OFF \
+		..
 	cmake --build .
 	cmake --build . --target install
 else
-	rm -rf build
+	rm -rf build/
 	rm -rf $PREFIX/*
 fi
